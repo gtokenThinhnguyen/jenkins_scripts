2ff6a46a6a520cebfb537355dcdc42bb8eb41572
gtokenThinhnguyen <thinhnguyen@gtoken.com>
2016-09-09 16:29:42 +0800

    add condition to check current time is in business hour

--- scripts_jenkin/stopSlaveInstances.sh
+++ scripts_jenkin/stopSlaveInstances.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
-readonly region='ap-southeast-1'
-output="$(aws ec2 describe-instances --filters 'Name=instance-state-name,Values=running' 'Name=tag:Type,Values=jenkin-slave' --region $region --query '[Reservations[*].Instances[*].InstanceId]' --output text)"
-echo $output | while read line;
-do
-if [ -n "$line" ]; then
-  aws ec2 stop-instances --region $region --instance-ids $line
+readonly startTime="000000"
+readonly endTime="130000"
+currentTime=`date +"%H%M%S"`
+if [[ "$currentTime" < "$startTime" && "$currentTime" > "$endTime" ]];
+then
+    readonly region='ap-southeast-1'
+    output="$(aws ec2 describe-instances --filters 'Name=instance-state-name,Values=running' 'Name=tag:Type,Values=jenkin-slave' --region $region --query '[Reservations[*].Instances[*].InstanceId]' --output text)"
+    echo $output | while read line;
+    do
+    if [ -n "$line" ]; then
+      aws ec2 stop-instances --region $region --instance-ids $line
+    else
+      echo "empty"
+    fi
+    done
 else
-  echo "empty"
+    echo "Do nothing"
 fi
-done
